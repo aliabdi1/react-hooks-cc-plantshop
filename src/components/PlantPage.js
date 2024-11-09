a278c4ce7ad61bf6e75d38f6612d1b466233cbb5
Ali Abdkadir <ali.abdikadir@student.moringaschool.com>
2024-11-09 16:13:32 +0300

    initial

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [SearchTerm, setSearchTerm] = useState("");
+
+  useEffect(()=> {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then((data) => setPlants(data));
+
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    setPlants([...plants,newPlant])
+  }
+
+  const handleSearchChange = (term) => {
+    setSearchTerm(term)
+  }
+
+  const filteredPlants = plants.filter((plants) => 
+    plants.name.toLowerCase().includes(SearchTerm.toLowerCase()));
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search setSearchTerm={setSearchTerm} onSearchChange={handleSearchChange}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
